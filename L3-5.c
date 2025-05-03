a467aaadb7ecf0a9477a2b1b9507ccf9dbeb4a4c
Rafaelfdias120 <rafaelfdias043@gmail.com>
2025-05-03 13:31:33 -0300

    a a

--- L3-5.c
+++ L3-5.c
@@ -1,6 +1,21 @@
 #include <stdio.h>
+#include <math.h>
 
 int main(){
 
+    int n, nq, nr;
+
+    printf("Digite um numero: ");
+    scanf("%d", &n);
     
+    if (n > 1)
+    {
+        nq = pow(n, 2);
+        nr = sqrt(n);
+
+        printf("Numero ao quadrado: %d\n", nq);
+        printf("Raiz quadrada do numero: %d\n", nr);
+    }
+    
+    return 0;
 }
