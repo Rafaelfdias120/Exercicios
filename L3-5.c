b88ce897d17e2d5a89d45ecff894871377e0aca0
Rafaelfdias120 <rafaelfdias043@gmail.com>
2025-05-03 19:37:08 -0300

    revisao

--- L3-5.c
+++ L3-5.c
@@ -2,5 +2,20 @@
 
 int main(){
 
+    int n, nq, nr;
+
+    printf("Digite um numero: ");
+    scanf("%d", &n);
+
+    if (n > 1)
+    {
+        nq = pow(n, 2);
+        nr = sqrt(n);
+
+        printf("Numero ao quadrado: %d\n", nq);
+        printf("Rai quadrada do numero: %d\n", nr);
+    }
+    
+    return 0;
     
 }
